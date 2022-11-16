ae78dbf63fc3504952266ad93e725e4482f69a14
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-16 19:31:38 +0100

    retry

--- src/contexts/circuit/context.tsx
+++ src/contexts/circuit/context.tsx
@@ -1,6 +1,6 @@
-import { Circuit } from "@hazae41/echalote";
+import { Circuit, Tor } from "@hazae41/echalote";
 import fallbacks from "assets/fallbacks.json";
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { randomOf } from "utils/array";
 import { ChildrenProps } from "utils/react/props";
 import { useTor } from "../tor/context";
@@ -12,37 +12,79 @@ export function useCircuit() {
   return useContext(CircuitContext)!
 }
 
-const middles = [
-  "D89267FB10BF625D31FF7687AF7D12B03BBF757C",
-  "23BAB4A9B1B7F553599CD81AED553FACB7B35210",
-  "FD449127D30D8F5D124653D9EF736EDF4A12B4DC",
-  "D3A1B7DEF370CBC6055F3FC540A518C8576D7570",
-]
-
-const exits = [
-  "4211FE6AA3991CFD9CD1CC897BD09C2CF73CF1F7",
-  "C8D207FE01D241F9AC86F2A2851CDC2E6998E51C",
-  "BC06A4AE847DDC23FD63082E388BB30924DAB4B6"
-]
-
 // const middles = [
 //   "D89267FB10BF625D31FF7687AF7D12B03BBF757C",
 //   "23BAB4A9B1B7F553599CD81AED553FACB7B35210",
 //   "FD449127D30D8F5D124653D9EF736EDF4A12B4DC",
 //   "D3A1B7DEF370CBC6055F3FC540A518C8576D7570",
-//   "51374C8DA459C67329FFD5C7502CCAE4194910CC",
-//   "31832D42A1B47E90970704FE6D7210D25FA1E5E3",
-//   "8F9CD937D0177BE8AC9E27D18604F93216DFA6A9",
 // ]
 
 // const exits = [
 //   "4211FE6AA3991CFD9CD1CC897BD09C2CF73CF1F7",
 //   "C8D207FE01D241F9AC86F2A2851CDC2E6998E51C",
-//   "BC06A4AE847DDC23FD63082E388BB30924DAB4B6",
-//   "4C3EF4B0C172F0C12891566E276F16D7DC07D049",
-//   "B2197C23A4FF5D1C49EE45BA7688BA8BCCD89A0B",
-//   "630F75D5AD741889C1BC46DC354A6320152A7B32",
+//   "BC06A4AE847DDC23FD63082E388BB30924DAB4B6"
 // ]
+
+const middles = [
+  "D89267FB10BF625D31FF7687AF7D12B03BBF757C",
+  "23BAB4A9B1B7F553599CD81AED553FACB7B35210",
+  "FD449127D30D8F5D124653D9EF736EDF4A12B4DC",
+  "D3A1B7DEF370CBC6055F3FC540A518C8576D7570",
+  "51374C8DA459C67329FFD5C7502CCAE4194910CC",
+  "31832D42A1B47E90970704FE6D7210D25FA1E5E3",
+  "8F9CD937D0177BE8AC9E27D18604F93216DFA6A9",
+]
+
+const exits = [
+  "4211FE6AA3991CFD9CD1CC897BD09C2CF73CF1F7",
+  "C8D207FE01D241F9AC86F2A2851CDC2E6998E51C",
+  "4C3EF4B0C172F0C12891566E276F16D7DC07D049",
+  "B2197C23A4FF5D1C49EE45BA7688BA8BCCD89A0B",
+  "630F75D5AD741889C1BC46DC354A6320152A7B32",
+]
+
+async function extendMiddle(circuit: Circuit) {
+  while (true)
+    try {
+      const middleid = randomOf(middles)!
+      const middle = fallbacks.find(it => it.id === middleid)!
+
+      // const middle = randomOf(fallbacks)!
+
+      console.log("middle", middle.id)
+      await circuit._extend(middle)
+
+      break
+    } catch (e: unknown) {
+      console.warn(e)
+    }
+}
+
+async function extendExit(circuit: Circuit) {
+  while (true)
+    try {
+      const exitid = randomOf(exits)!
+      const exit = fallbacks.find(it => it.id === exitid)!
+
+      // const exit = randomOf(fallbacks.filter(it => it.exit))!
+
+      console.log("exit", exit.id)
+      await circuit._extend(exit)
+
+      break
+    } catch (e: unknown) {
+      console.warn(e)
+    }
+}
+
+async function createCircuit(tor: Tor) {
+  if (!tor) return
+
+  const circuit = await tor.create()
+  await extendMiddle(circuit)
+  await extendExit(circuit)
+  return circuit
+}
 
 export function CircuitProvider(props: ChildrenProps) {
   const { children } = props
@@ -51,33 +93,9 @@ export function CircuitProvider(props: ChildrenProps) {
 
   const [circuit, setCircuit] = useState<Circuit>()
 
-  const onLoad = useCallback(async () => {
-    if (!tor) return
-
-    const circuit = await tor.create()
-
-    const middleid = randomOf(middles)!
-    const middle = fallbacks.find(it => it.id === middleid)!
-
-    // const middle = randomOf(fallbacks)!
-
-    console.log("middle", middle.id)
-    await circuit._extend(middle)
-
-    const exitid = randomOf(exits)!
-    const exit = fallbacks.find(it => it.id === exitid)!
-
-    // const exit = randomOf(fallbacks.filter(it => it.exit))!
-
-    console.log("exit", exit.id)
-    await circuit._extend(exit)
-
-    setCircuit(circuit)
-  }, [tor])
-
   useEffect(() => {
-    if (tor) onLoad()
-  }, [tor, onLoad])
+    if (tor) createCircuit(tor).then(setCircuit)
+  }, [tor])
 
   if (!circuit)
     return <div className="p-md flex flex-col items-center">
--- src/contexts/tor/context.tsx
+++ src/contexts/tor/context.tsx
@@ -1,14 +1,33 @@
 import { TlsOverHttp, Tor } from "@hazae41/echalote";
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { ChildrenProps } from "utils/react/props";
 
-async function http() {
-  const headers = new Headers({ "x-session-id": crypto.randomUUID() })
-  const request = new Request("https://meek.bamsoftware.com/", { headers })
+async function createTlsOverHttp() {
+  while (true)
+    try {
+      const headers = new Headers({ "x-session-id": crypto.randomUUID() })
+      const request = new Request("https://meek.bamsoftware.com/", { headers })
 
-  const tls = new TlsOverHttp(request)
-  await tls.open()
-  return tls
+      const tls = new TlsOverHttp(request)
+      await tls.open()
+      return tls
+    } catch (e: unknown) {
+      console.warn(e)
+    }
+}
+
+async function createTor() {
+  const tls = await createTlsOverHttp()
+
+  while (true)
+    try {
+      const tor = new Tor(tls)
+      await tor.init()
+      await tor.handshake()
+      return tor
+    } catch (e: unknown) {
+      console.warn(e)
+    }
 }
 
 export const TorContext =
@@ -23,17 +42,9 @@ export function TorProvider(props: ChildrenProps) {
 
   const [tor, setTor] = useState<Tor>()
 
-  const onLoad = useCallback(async () => {
-    const tls = await http()
-    const tor = new Tor(tls)
-    await tor.init()
-    await tor.handshake()
-    setTor(tor)
-  }, [])
-
   useEffect(() => {
-    onLoad()
-  }, [onLoad])
+    createTor().then(setTor)
+  }, [])
 
   if (!tor)
     return <div className="p-md flex flex-col items-center">
