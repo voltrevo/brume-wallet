c03405f4b8f0c4d7af9e1e3f701e478a632c2461
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-08 10:22:41 +0100

    fix

--- pages/wallet/[address].tsx
+++ pages/wallet/[address].tsx
@@ -1,6 +1,11 @@
 import { Types } from "@/libs/types/types";
 import { WalletPage } from "@/mods/entities/wallets/page";
+import { GetStaticPaths } from "next";
 import { useRouter } from "next/router";
+
+export const getStaticPaths: GetStaticPaths = async function () {
+  return { paths: [], fallback: "blocking" }
+}
 
 export default function Page() {
   const router = useRouter()
