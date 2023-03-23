6b8f4e753ad9544f436641b30ec1c1e3a04cc94d
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-23 20:58:23 +0100

    fix extension

--- src/mods/overlay/overlay.tsx
+++ src/mods/overlay/overlay.tsx
@@ -1,11 +1,15 @@
 import { ChildrenProps } from "@/libs/react/props/children";
+import { useEffect, useState } from "react";
 
 export function Overlay(props: ChildrenProps) {
   const { children } = props
 
-  const extension = typeof location !== "undefined"
-    ? location.protocol === "chrome-extension:"
-    : false
+  const [extension, setExtension] = useState(false)
+
+  useEffect(() => {
+    if (location.protocol === "chrome-extension:")
+      setExtension(true)
+  }, [])
 
   if (extension)
     return <main className="h-[600px] w-[400px] p-4">
