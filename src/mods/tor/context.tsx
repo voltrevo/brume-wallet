087273bffa75b15b843636628f04ff9f46c492f7
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-26 18:19:19 +0200

    update

--- src/mods/tor/context.tsx
+++ src/mods/tor/context.tsx
@@ -1,7 +1,10 @@
 import fallbacks from "@/assets/fallbacks.json";
 import { useAsyncMemo } from "@/libs/react/memo";
 import { ChildrenProps } from "@/libs/react/props/children";
+import { Berith } from "@hazae41/berith";
 import { createWebSocketSnowflakeStream, TorClientDuplex } from "@hazae41/echalote";
+import { Ed25519 } from "@hazae41/ed25519";
+import { X25519 } from "@hazae41/x25519";
 import { createContext, useContext } from "react";
 
 export const TorContext =
@@ -15,9 +18,14 @@ export function TorProvider(props: ChildrenProps) {
   const { children } = props
 
   const tor = useAsyncMemo(async () => {
+    await Berith.initBundledOnce()
+
+    const ed25519 = Ed25519.fromBerith(Berith)
+    const x25519 = X25519.fromBerith(Berith)
+
     const tcp = await createWebSocketSnowflakeStream("wss://snowflake.bamsoftware.com/")
 
-    return new TorClientDuplex(tcp, { fallbacks })
+    return new TorClientDuplex(tcp, { fallbacks, ed25519, x25519 })
   }, [])
 
   return <TorContext.Provider value={tor}>
