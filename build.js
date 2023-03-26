566be9ac1781d677feceda8af44d9b81d1bc7242
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-26 11:33:50 +0200

    fix icon

--- build.js
+++ build.js
@@ -5,14 +5,16 @@ function* walkSync(dir) {
   const files = fs.readdirSync(dir, { withFileTypes: true });
   for (const file of files) {
     if (file.isDirectory()) {
-      yield* walkSync(path.join(dir, file.name));
+      yield* walkSync(path.join(dir, file.name))
     } else {
-      yield path.join(dir, file.name);
+      yield path.join(dir, file.name)
     }
   }
 }
 
 for (const filePath of walkSync("./out")) {
-  const content = fs.readFileSync(filePath, "utf8")
-  fs.writeFileSync(filePath, content.replaceAll("_next", "next"), "utf8")
+  if (filePath.endsWith(".js") || filePath.endsWith(".html")) {
+    const content = fs.readFileSync(filePath, "utf8")
+    fs.writeFileSync(filePath, content.replaceAll("_next", "next"), "utf8")
+  }
 }
