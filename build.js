849ba3b3a1e5c457f48cf14edd2f1f4310252562
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-03-26 11:26:59 +0200

    fix

---
+++ build.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+function* walkSync(dir) {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* walkSync(path.join(dir, file.name));
+    } else {
+      yield path.join(dir, file.name);
+    }
+  }
+}
+
+for (const filePath of walkSync("./out")) {
+  const content = fs.readFileSync(filePath, "utf8")
+  fs.writeFileSync(filePath, content.replaceAll("_next", "next"), "utf8")
+}
