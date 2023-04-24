66339a649042327f9f8aa12e167d805236aad4d3
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-04-24 17:45:24 +0200

    circuits mutex

--- src/libs/tor/sessions/pool.ts
+++ src/libs/tor/sessions/pool.ts
@@ -3,13 +3,11 @@ import { Mutex } from "@hazae41/mutex"
 import { Pool } from "@hazae41/piscine"
 import { createSession, Session } from "./session"
 
-export function createSessionPool(url: URL, circuits: Pool<Circuit>) {
-  const { capacity } = circuits
-
-  const mutex = new Mutex(undefined)
+export function createSessionPool(url: URL, circuits: Mutex<Pool<Circuit>>) {
+  const { capacity } = circuits.inner
 
   return new Pool<Session>(async ({ pool, signal }) => {
-    const circuit = await mutex.lock(async () => {
+    const circuit = await circuits.lock(async (circuits) => {
       const circuit = await circuits.cryptoRandom()
       circuits.delete(circuit)
       return circuit
--- src/mods/tor/circuits/context.tsx
+++ src/mods/tor/circuits/context.tsx
@@ -1,11 +1,12 @@
 import { ChildrenProps } from "@/libs/react/props/children";
 import { Circuit, createCircuitPool } from "@hazae41/echalote";
+import { Mutex } from "@hazae41/mutex";
 import { Pool } from "@hazae41/piscine";
 import { createContext, useContext, useMemo } from "react";
 import { useTor } from "../context";
 
 export const CircuitsContext =
-  createContext<Pool<Circuit> | undefined>(undefined)
+  createContext<Mutex<Pool<Circuit>> | undefined>(undefined)
 
 export function useCircuits() {
   return useContext(CircuitsContext)
@@ -19,7 +20,7 @@ export function CircuitsProvider(props: ChildrenProps) {
   const circuits = useMemo(() => {
     if (!tor) return
 
-    return createCircuitPool(tor, {})
+    return new Mutex(createCircuitPool(tor, {}))
   }, [tor])
 
   return <CircuitsContext.Provider value={circuits}>
