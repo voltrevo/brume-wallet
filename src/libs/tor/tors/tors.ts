cb89c3d954a9e7a0bab17c650efc69123365a524
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-09 21:59:35 +0200

    tor pool

--- src/libs/tor/tors/tors.ts
+++ src/libs/tor/tors/tors.ts
@@ -1,17 +1,38 @@
-import { Creator, TorClientDuplex, TorClientParams, createPooledTor, createWebSocketSnowflakeStream, tryCreateLoop } from "@hazae41/echalote"
+import { Fallback, TorClientDuplex, TorClientParams, createPooledTor, createWebSocketSnowflakeStream } from "@hazae41/echalote"
 import { Mutex } from "@hazae41/mutex"
-import { Pool, PoolParams } from "@hazae41/piscine"
+import { Cancel, Creator, Pool, PoolParams, tryCreateLoop } from "@hazae41/piscine"
 import { Ok, Result } from "@hazae41/result"
 
-export async function tryCreateTor(params: TorClientParams): Promise<Result<TorClientDuplex, Error>> {
-  const tcp = await createWebSocketSnowflakeStream("wss://snowflake.torproject.net/")
-  const tor = new TorClientDuplex(tcp, params)
-
-  return tor.tryWait().then(r => r.mapSync(() => tor))
+export interface CreateTorParams extends Omit<TorClientParams, "fallbacks"> {
+  fallbacks: Result<Fallback[], Error>
 }
 
-export function createTorPool<CreateError>(tryCreate: Creator<TorClientDuplex, CreateError>, params: PoolParams = {}) {
-  return new Mutex(new Pool<TorClientDuplex, Error | CreateError>(async (params) => {
+export async function tryCreateTor2(params: CreateTorParams): Promise<Result<TorClientDuplex, Cancel<Error>>> {
+  return await Result.unthrow(async t => {
+    const fallbacks = params.fallbacks.mapErrSync(Cancel.new).throw(t)
+
+    const tcp = await createWebSocketSnowflakeStream("wss://snowflake.torproject.net/")
+    const tor = new TorClientDuplex(tcp, { ...params, fallbacks })
+
+    tor.tryWait().then(r => r.mapErrSync(Cancel.new).throw(t))
+
+    return new Ok(tor)
+  })
+}
+
+export async function tryCreateTor(params: TorClientParams): Promise<Result<TorClientDuplex, Cancel<Error>>> {
+  return await Result.unthrow(async t => {
+    const tcp = await createWebSocketSnowflakeStream("wss://snowflake.torproject.net/")
+    const tor = new TorClientDuplex(tcp, params)
+
+    tor.tryWait().then(r => r.mapErrSync(Cancel.new).throw(t))
+
+    return new Ok(tor)
+  })
+}
+
+export function createTorPool<CancelError, RetryError>(tryCreate: Creator<TorClientDuplex, CancelError, RetryError>, params: PoolParams = {}) {
+  return new Mutex(new Pool<TorClientDuplex, Error | CancelError>(async (params) => {
     return await Result.unthrow(async t => {
       const tor = await tryCreateLoop(tryCreate, params).then(r => r.throw(t))
 
--- src/mods/background/service_worker/index.ts
+++ src/mods/background/service_worker/index.ts
@@ -1,3 +1,11 @@
+import { createTorPool, tryCreateTor2 } from "@/libs/tor/tors/tors"
+import { Berith } from "@hazae41/berith"
+import { Fallback } from "@hazae41/echalote"
+import { Ed25519 } from "@hazae41/ed25519"
+import { Morax } from "@hazae41/morax"
+import { Catched, Err, Ok, Result } from "@hazae41/result"
+import { Sha1 } from "@hazae41/sha1"
+import { X25519 } from "@hazae41/x25519"
 import { clientsClaim } from 'workbox-core'
 import { precacheAndRoute } from "workbox-precaching"
 
@@ -19,3 +27,36 @@ if (self.__WB_PRODUCTION && !location.protocol.endsWith("extension:")) {
       self.skipWaiting()
   })
 }
+
+async function tryFetch<T>(url: string): Promise<Result<T, Error>> {
+  try {
+    const res = await fetch(url)
+
+    if (!res.ok)
+      return new Err(new Error(await res.text()))
+    return new Ok(await res.json() as T)
+  } catch (e: unknown) {
+    return new Err(Catched.from(e))
+  }
+}
+
+const FALLBACKS_URL = "https://raw.githubusercontent.com/hazae41/echalote/master/tools/fallbacks/fallbacks.json"
+
+async function main() {
+  await Berith.initBundledOnce()
+  await Morax.initBundledOnce()
+
+  const ed25519 = Ed25519.fromBerith(Berith)
+  const x25519 = X25519.fromBerith(Berith)
+  const sha1 = Sha1.fromMorax(Morax)
+
+  const fallbacks = await tryFetch<Fallback[]>(FALLBACKS_URL)
+
+  const tors = createTorPool(async () => {
+    return await tryCreateTor2({ fallbacks, ed25519, x25519, sha1 })
+  }, { capacity: 3 })
+
+  console.log(tors)
+}
+
+main()
