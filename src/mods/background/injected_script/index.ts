0a57d4ed4e9cad597becde99be4929732eacf136
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-11 17:15:19 +0200

    provider

--- src/mods/background/injected_script/index.ts
+++ src/mods/background/injected_script/index.ts
@@ -1,1 +1,34 @@
-setInterval(() => console.log("injected script2"), 1000)
+import { Panic } from "@hazae41/result"
+
+declare global {
+  interface Window {
+    ethereum?: Provider
+  }
+}
+
+interface JsonRpcRequestInit {
+  method: string
+  params?: unknown[]
+}
+
+class Provider {
+
+  get isConnected() {
+    return true
+  }
+
+  async request(request: JsonRpcRequestInit) {
+    if (request.method === "eth_requestAccounts")
+      return ["0x39dfd20386F5d17eBa42763606B8c704FcDd1c1D"]
+    if (request.method === "eth_chainId")
+      return "0x1"
+    throw new Panic(`Invalid JSON-RPC request ${request.method}`)
+  }
+
+  on(event: string, listener: () => void) {
+
+  }
+
+}
+
+window.ethereum = new Provider()
