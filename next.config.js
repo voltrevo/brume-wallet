f5d987507b6e07d3a5193628d0d72a1c73469654
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-24 13:27:11 +0200

    no sourcemap

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ const nextConfig = {
   reactStrictMode: false, // TODO
   swcMinify: true,
   output: "export",
-  productionBrowserSourceMaps: true,
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
     return 'my-build-id'
