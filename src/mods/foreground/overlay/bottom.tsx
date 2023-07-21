229892c3057fbc0a96aadf97ad113bd132f5fb40
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-21 18:55:07 +0200

    fix bottom

--- src/mods/foreground/overlay/bottom.tsx
+++ src/mods/foreground/overlay/bottom.tsx
@@ -18,7 +18,7 @@ export function Bottom() {
 
   return <>
     <div className="h-16" />
-    <nav className="fixed bottom-0 w-full h-16 bg-paper flex items-center">
+    <nav className="fixed bottom-0 left-0 w-full h-16 bg-paper flex items-center">
       <Button.Naked className="grow text-contrast aria-selected:text-default"
         aria-selected={path.pathname === "/" || path.pathname === "/wallets"}
         onClick={wallets}>
