5e4c33dda02b19d53e6dfd10d239f920e3b5d654
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-22 11:16:12 +0200

    destroy all requests

--- src/mods/foreground/entities/requests/all/page.tsx
+++ src/mods/foreground/entities/requests/all/page.tsx
@@ -1,21 +1,46 @@
 /* eslint-disable @next/next/no-img-element */
+import { Button } from "@/libs/components/button"
+import { Errors } from "@/libs/errors/errors"
+import { Outline } from "@/libs/icons/icons"
+import { RpcErr, RpcError } from "@/libs/rpc"
 import { qurl } from "@/libs/url/url"
 import { AppRequest } from "@/mods/background/service_worker/entities/requests/data"
+import { useBackground } from "@/mods/foreground/background/context"
 import { PageBody, PageHeader } from "@/mods/foreground/components/page/header"
 import { Page } from "@/mods/foreground/components/page/page"
+import { UserRejectionError } from "@/mods/foreground/errors/errors"
 import { Path } from "@/mods/foreground/router/path"
+import { Ok, Result } from "@hazae41/result"
 import { useCallback } from "react"
 import { useOrigin } from "../../origins/data"
 import { useAppRequest } from "../data"
 import { useAppRequests } from "./data"
 
 export function RequestsPage() {
-  const requests = useAppRequests()
+  const background = useBackground()
+
+  const requestsQuery = useAppRequests()
+  const requestsData = requestsQuery.data?.inner
+
+  const clean = useCallback(async () => {
+    return await Result.unthrow<Result<void, Error>>(async t => {
+      if (requestsData == null)
+        return Ok.void()
+
+      for (const { id } of requestsData)
+        await background.tryRequest({
+          method: "popup_data",
+          params: [new RpcErr(id, RpcError.from(new UserRejectionError()))]
+        }).then(r => r.throw(t))
+
+      return Ok.void()
+    }).then(r => r.inspectErrSync(e => alert(Errors.toString(e))))
+  }, [background, requestsData])
 
   const Body =
     <PageBody>
       <div className="flex flex-col gap-2">
-        {requests.data?.inner.map(request =>
+        {requestsQuery.data?.inner.map(request =>
           <RequestRow
             key={request.id}
             request={request} />)}
@@ -23,7 +48,12 @@ export function RequestsPage() {
     </PageBody>
 
   const Header =
-    <PageHeader title="Requests" />
+    <PageHeader title="Requests">
+      <Button.Naked className="icon-xl hovered-or-clicked-or-focused:scale-105 transition"
+        onClick={clean}>
+        <Outline.TrashIcon className="icon-sm" />
+      </Button.Naked>
+    </PageHeader>
 
   return <Page>
     {Header}
