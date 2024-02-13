81151fffc17e5502ac73bb175b28286e1e211601
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-13 23:24:55 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,36 @@ module.exports = {
   },
   safelist: [
     {
-      pattern: /(text|bg|border)-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(400|500)(\/90)?/,
-      variants: ["dark", "enabled:hover", "dark:enabled:hover", "focus-visible", "dark:focus-visible"]
+      pattern: /text-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-400/,
+      variants: []
+    },
+    {
+      pattern: /text-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500/,
+      variants: ["dark"]
+    },
+    {
+      pattern: /bg-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-400/,
+      variants: []
+    },
+    {
+      pattern: /bg-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500/,
+      variants: ["dark"]
+    },
+    {
+      pattern: /bg-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-400\/90/,
+      variants: ["enabled:hover"]
+    },
+    {
+      pattern: /bg-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500\/90/,
+      variants: ["dark:enabled:hover"]
+    },
+    {
+      pattern: /outline-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-400/,
+      variants: ["focus-visible"]
+    },
+    {
+      pattern: /outline-(red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500/,
+      variants: ["dark:focus-visible"]
     },
   ],
 };
