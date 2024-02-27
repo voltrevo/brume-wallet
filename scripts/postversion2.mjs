d0e723ef1dcc53b870869b428cc2b5f627865ab3
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-27 16:38:21 +0100

    altstore

---
+++ scripts/postversion2.mjs
@@ -0,0 +1,20 @@
+import { readFileSync, statSync, writeFileSync } from "fs";
+
+const version = JSON.parse(readFileSync("./package.json", "utf8")).version;
+const versionCode = Number(version.replaceAll(".", "")).toString()
+
+{
+  const json = JSON.parse(readFileSync("./altstore.json", "utf8"));
+
+  json.apps[0].versions.push({
+    "version": version,
+    "date": new Date().toISOString(),
+    "localizedDescription": "Improvements and fixes. See GitHub for more details.",
+    "downloadURL": `https://github.com/brumewallet/wallet/raw/v${version}/dist/ios-and-ipados.ipa`,
+    "size": statSync("./dist/ios-and-ipados.ipa").size,
+    "sha256": Buffer.from(crypto.subtle.digest("SHA-256", readFileSync("./dist/ios-and-ipados.ipa"))).toString("hex"),
+    "minOSVersion": "15.0"
+  })
+
+  writeFileSync("./altstore.json", JSON.stringify(json, null, 2));
+}
