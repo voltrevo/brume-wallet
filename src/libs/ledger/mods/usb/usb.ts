34a5308484c450c749057a82ad462c6c1688b581
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-05-09 11:58:25 +0700

    hid

--- src/libs/ledger/mods/usb/usb.ts
+++ src/libs/ledger/mods/usb/usb.ts
@@ -2,7 +2,7 @@ import { ApduRequest, ApduRequestInit, ApduResponse } from "@hazae41/apdu"
 import { Opaque, Readable, Writable } from "@hazae41/binary"
 import { Bytes } from "@hazae41/bytes"
 import { Err, Ok, Result } from "@hazae41/result"
-import { HIDContainer, HIDFrame } from "../../libs/hid/frame"
+import { HIDContainer, HIDFrame } from "../hid/frame"
 
 export const VENDOR_ID = 0x2c97
 export const PACKET_SIZE = 64
