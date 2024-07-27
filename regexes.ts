daa6f461d183e0a5beed3c86a41fffec7dd8b368
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-27 13:37:05 +0700

    regexes

--- regexes.ts
+++ regexes.ts
@@ -9,7 +9,7 @@ function getRegexes(text: string) {
   let slice = text
 
   while (true) {
-    const match = slice.match(/((return\s*)|(:\s*)|(=\s*)|(\(\s*))(\/.+\/[a-z]*)/)
+    const match = slice.match(/((return\s*)|(\:\s*)|(\=\s*)|(\(\s*))(\/((?![*+?])(?:[^\r\n\[\/\\]|\\.|\[(?:[^\r\n\]\\]|\\.)*\])+)\/[gimsuy]{0,6})((\s*\.)|(\s*\,)|(\s*\;)|(\s*\)))/)
 
     if (match == null)
       break
@@ -18,56 +18,20 @@ function getRegexes(text: string) {
 
     const [raw, _pre, _ret, _colon, _equals, _paren, regex] = match
 
-    console.log("a", raw.slice(0, 100))
+    console.log("!a!", regex)
 
-    let laziest: RegExp | null = null
-
-    for (let i = 1; i < regex.length;) {
-      const aaa = regex.indexOf("/", i)
-
-      if (aaa == -1)
-        break
-
-      const bbb = regex.indexOf("/", aaa + 1)
-
-      console.log("a2", regex.slice(0, bbb + 1).slice(0, 100), regex.slice(0, aaa + 1).length, regex.length)
-
-      try {
-        laziest = eval(`new RegExp(${regex.slice(0, aaa + 1)})`)
-        break
-      } catch (e) {
-        i = aaa + 1
-        continue
-      }
-    }
-
-    if (laziest == null) {
+    try {
+      eval(`new RegExp(${regex})`)
+    } catch (e) {
       index += match.index + 1
       slice = text.slice(index)
       continue
     }
 
-    console.log("a3", raw.slice(0, 100))
+    const start = index - 1 + match.index + raw.length - regex.length
+    regexes.push([start, start + regex.length])
 
-    const flagsi = index + match.index + raw.length - regex.length + String(laziest).length
-    const flagsm = text.slice(flagsi).match(/^[a-z]*/)
-
-    if (flagsm != null) {
-      const [flags] = flagsm
-
-      try {
-        laziest = new RegExp(laziest, flags)
-      } catch (e) {
-        index += match.index + 1
-        slice = text.slice(index)
-        continue
-      }
-    }
-
-    const start = index + match.index + raw.length - regex.length
-    regexes.push([start, start + String(laziest).length])
-
-    console.log("b", text.slice(start, start + String(laziest).length))
+    console.log("!b!", text.slice(start, start + regex.length))
 
     index += match.index + 1
     slice = text.slice(index)
