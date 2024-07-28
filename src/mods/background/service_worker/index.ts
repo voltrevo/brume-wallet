2b6c03493116c04ad34c0a1b0ed7aaff24e9a1d1
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-28 22:52:37 +0700

    dd

--- src/mods/background/service_worker/index.ts
+++ src/mods/background/service_worker/index.ts
@@ -75,7 +75,7 @@ if (isWebsite() || isAndroidApp())
 declare const FILES: [string, string][]
 
 if (isProdWebsite()) {
-  console.log("Service worker")
+  console.log("2")
 
   const cache = new Immutable.Cache(new Map(FILES))
 
