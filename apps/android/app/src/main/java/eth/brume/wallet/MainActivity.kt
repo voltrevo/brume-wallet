a67b3f06bc08e2b361461d957e6f692b50107101
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-07-30 00:23:09 +0700

    android logs

--- apps/android/app/src/main/java/eth/brume/wallet/MainActivity.kt
+++ apps/android/app/src/main/java/eth/brume/wallet/MainActivity.kt
@@ -1,8 +1,11 @@
 package eth.brume.wallet
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.ConsoleMessage
 import android.webkit.ServiceWorkerClient
 import android.webkit.ServiceWorkerController
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import com.getcapacitor.BridgeActivity
@@ -16,5 +19,12 @@ class MainActivity: BridgeActivity() {
                 return bridge.localServer.shouldInterceptRequest(request)
             }
         })
+
+        bridge.webView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(message: ConsoleMessage): Boolean {
+                Log.d("WebView", message.message())
+                return true
+            }
+        }
     }
 }
