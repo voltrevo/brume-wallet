23678699ee7eeece1e04fc2dddb8fd15a90712d6
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-08 00:02:36 +0700

    fix

--- src/libs/url/url.ts
+++ src/libs/url/url.ts
@@ -1,11 +1,17 @@
 import { Nullable } from "@hazae41/option"
 
-export function urlOf(pathOrUrl: string | URL, search: Record<string, Nullable<any>> = {}) {
-  const url = new URL(pathOrUrl, location.href)
-  const entries = Object.entries(search).filter(([_, v]) => v != null)
-  url.search = new URLSearchParams(entries).toString()
+export function urlOf(hrefOrUrl: string | URL, search: Record<string, Nullable<any>> = {}) {
+  const url = new URL(hrefOrUrl, location.href)
+
+  for (const key in search)
+    if (search[key] == null)
+      url.searchParams.delete(key)
+    else
+      url.searchParams.set(key, search[key])
+
   return url
 }
+
 
 export function pathOf(pathOrUrl: string | URL) {
   const url = new URL(pathOrUrl, location.href)
