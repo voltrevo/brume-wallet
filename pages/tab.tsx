d0246e4299cc44427e3fb651723d1a814fd9b3a1
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-14 00:14:27 +0700

    fix

--- pages/tab.tsx
+++ pages/tab.tsx
@@ -1,28 +1,11 @@
 import { browser, BrowserError } from "@/libs/browser/browser";
 import { isSafariExtension } from "@/libs/platform/platform";
-import { useBackgroundContext } from "@/mods/foreground/background/context";
 import { NavBar } from "@/mods/foreground/overlay/navbar";
 import { Nullable } from "@hazae41/option";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { isEmptyHash } from "./popup";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Action() {
-  const background = useBackgroundContext().unwrap()
-
-  const getHashOrThrow = useCallback(async () => {
-    return await background.requestOrThrow<string>({
-      method: "brume_getPath"
-    }).then(r => r.unwrap())
-  }, [background])
-
-  const setHashOrThrow = useCallback(async (hash: string) => {
-    await background.requestOrThrow<void>({
-      method: "brume_setPath",
-      params: [hash]
-    }).then(r => r.unwrap())
-  }, [background])
-
-  const [hash, setHash] = useState<string>()
+  const [hash, setHash] = useState<string>(location.hash)
 
   useEffect(() => {
     const onHashChange = () => setHash(location.hash)
@@ -31,53 +14,37 @@ export default function Action() {
     return () => removeEventListener("hashchange", onHashChange)
   }, [])
 
-  const initHashOrThrow = useCallback(async () => {
-    if (!isEmptyHash(location.hash)) {
-      setHash(location.hash)
-      return
-    }
-
-    const hash = await getHashOrThrow()
-
-    location.hash = hash
-    setHash(location.hash)
-  }, [getHashOrThrow])
-
-  useEffect(() => {
-    initHashOrThrow().catch(console.error)
-  }, [initHashOrThrow])
-
-  useEffect(() => {
-    if (hash == null)
-      return
-    setHashOrThrow(hash).catch(console.error)
-  }, [setHashOrThrow, hash])
-
   const url = useMemo(() => {
-    if (hash == null)
-      return
-    const url = new URL(BrowserError.runOrThrowSync(() => browser.runtime.getURL("/index.html")))
-    url.hash = hash
-    return url
-  }, [hash])
+    return new URL(BrowserError.runOrThrowSync(() => browser.runtime.getURL("/index.html")))
+  }, [])
 
   const [iframe, setIframe] = useState<Nullable<HTMLIFrameElement>>()
 
-  useEffect(() => {
+  const subwindow = useMemo(() => {
     if (iframe == null)
       return
     if (iframe.contentWindow == null)
       return
-    const subwindow = iframe.contentWindow
+    return iframe.contentWindow
+  }, [iframe])
+
+  useEffect(() => {
+    if (subwindow == null)
+      return
 
     const onSubwindowHashChange = () => location.hash = subwindow.location.hash
 
     subwindow.addEventListener("hashchange", onSubwindowHashChange, { passive: true })
     return () => subwindow.removeEventListener("hashchange", onSubwindowHashChange)
-  }, [iframe])
+  }, [subwindow])
 
-  if (url == null)
-    return null
+  useEffect(() => {
+    if (subwindow == null)
+      return
+    setTimeout(() => {
+      subwindow.location.hash = hash
+    }, 100)
+  }, [subwindow, hash])
 
   return <main id="main" className="p-safe h-full w-full flex flex-col overflow-hidden">
     {isSafariExtension() && <NavBar />}
