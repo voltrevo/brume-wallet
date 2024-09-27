88f98d3a961dd042496bb95578ac8ed004eb94d5
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-09-27 21:07:17 +0700

    clean

--- src/libs/gardien/mods/guards/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./abstracts"
-export * from "./arrays"
-export * from "./equalities"
-export * from "./logicals"
-export * from "./nullables"
-export * from "./primitives"
-export * from "./records"
-
