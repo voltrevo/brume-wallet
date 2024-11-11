1904531e0ab5dd0af10ad357e2a13f005ec7ee3a
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-11-11 12:46:45 +0700

    d

--- src/mods/background/service_worker/context.ts
+++ src/mods/background/service_worker/context.ts
@@ -22,90 +22,42 @@ export namespace BgEthereumContext {
       const { brume } = ethereum
 
       const circuits = Option.wrap(brume[ethereum.chain.chainId]).getOrThrow()
+      const circuit = await circuits.get().getCryptoRandomOrThrow(parentSignal)
 
-      async function runWithCircuitOrThrow(index: number) {
-        const circuit = await circuits.get().getCryptoRandomOrThrow(parentSignal)
+      async function runWithTargetOrThrow(index: number) {
+        const target = await circuit.get().getOrThrow(index, parentSignal)
 
-        async function runWithTargetOrThrow(index: number) {
-          const target = await circuit.get().getOrThrow(index, parentSignal)
+        const { counter, connection } = target
+        const request = counter.prepare(init)
 
-          const { counter, connection } = target
-          const request = counter.prepare(init)
+        if (connection.isURL()) {
+          const { url, circuit } = connection
 
-          if (connection.isURL()) {
-            const { url, circuit } = connection
+          const signal = AbortSignal.any([AbortSignal.timeout(ping.value * 9), parentSignal])
+          const response = await TorRpc.fetchWithCircuitOrThrow<T>(url, { ...request, circuit, signal })
 
-            const signal = AbortSignal.any([AbortSignal.timeout(ping.value * 9), parentSignal])
-            const response = await TorRpc.fetchWithCircuitOrThrow<T>(url, { ...request, circuit, signal })
+          console.log(`Fetched ${request.method} on ${ethereum.chain.name}`, response)
 
-            console.log(`Fetched ${request.method} on ${ethereum.chain.name}`, response)
-
-            return Fetched.rewrap(response)
-          }
-
-          if (connection.isWebSocket()) {
-            const { socket, cooldown } = connection
-
-            await cooldown
-
-            const signal = AbortSignal.any([AbortSignal.timeout(ping.value * 9), parentSignal])
-            const response = await TorRpc.fetchWithSocketOrThrow<T>(socket, request, signal)
-
-            console.log(`Fetched ${request.method} on ${ethereum.chain.name}`, response)
-
-            return Fetched.rewrap(response)
-          }
-
-          return connection satisfies never
+          return Fetched.rewrap(response)
         }
 
-        const promises = Array.from({ length: circuit.get().size }, (_, i) => runWithTargetOrThrow(i))
+        if (connection.isWebSocket()) {
+          const { socket, cooldown } = connection
 
-        const results = await Promise.allSettled(promises)
+          await cooldown
 
-        const fetcheds = new Map<string, Fetched<T, Error>>()
-        const counters = new Map<string, number>()
+          const signal = AbortSignal.any([AbortSignal.timeout(ping.value * 9), parentSignal])
+          const response = await TorRpc.fetchWithSocketOrThrow<T>(socket, request, signal)
 
-        for (const result of results) {
-          if (result.status === "rejected")
-            continue
-          if (result.value.isErr())
-            continue
-          if (init?.noCheck)
-            return result.value
-          const raw = JSON.stringify(result.value.inner)
-          const previous = Option.wrap(counters.get(raw)).getOr(0)
-          counters.set(raw, previous + 1)
-          fetcheds.set(raw, result.value)
+          console.log(`Fetched ${request.method} on ${ethereum.chain.name}`, response)
+
+          return Fetched.rewrap(response)
         }
 
-        /**
-         * One truth -> return it
-         * Zero truth -> throw AggregateError
-         */
-        if (counters.size < 2)
-          return await Promise.any(promises)
-
-        console.warn(`Different results from multiple connections for ${init.method} on ${ethereum.chain.name}`, { fetcheds })
-
-        /**
-         * Sort truths by occurence
-         */
-        const sorteds = [...Maps.entries(counters)].sort((a, b) => b.value - a.value)
-
-        /**
-         * Two concurrent truths
-         */
-        if (sorteds[0].value === sorteds[1].value) {
-          console.warn(`Could not choose truth for ${init.method} on ${ethereum.chain.name}`)
-          const random = Math.round(Math.random())
-          return fetcheds.get(sorteds[random].key)!
-        }
-
-        return fetcheds.get(sorteds[0].key)!
+        return connection satisfies never
       }
 
-      const promises = Array.from({ length: 3 }, (_, i) => runWithCircuitOrThrow(i))
+      const promises = Array.from({ length: circuit.get().size }, (_, i) => runWithTargetOrThrow(i))
 
       const results = await Promise.allSettled(promises)
 
@@ -132,7 +84,7 @@ export namespace BgEthereumContext {
       if (counters.size < 2)
         return await Promise.any(promises)
 
-      console.warn(`Different results from multiple circuits for ${init.method} on ${ethereum.chain.name}`, { fetcheds })
+      console.warn(`Different results from multiple connections for ${init.method} on ${ethereum.chain.name}`, { fetcheds })
 
       /**
        * Sort truths by occurence
