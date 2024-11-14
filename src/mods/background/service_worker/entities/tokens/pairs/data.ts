10ff718f8523ff8b6328504f56f4b7573b78c457
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-11-14 22:12:13 +0700

    price

--- src/mods/background/service_worker/entities/tokens/pairs/data.ts
+++ src/mods/background/service_worker/entities/tokens/pairs/data.ts
@@ -1,14 +1,57 @@
 import { PairAbiV2, PairAbiV3 } from "@/libs/abi/pair.abi"
-import { PairData } from "@/libs/ethereum/mods/chain"
+import { PairData, tokenByAddress } from "@/libs/ethereum/mods/chain"
 import { UniswapV2 } from "@/libs/uniswap"
 import { Abi, Fixed, ZeroHexString } from "@hazae41/cubane"
-import { Data, Fetched, FetcherMore, QueryStorage, createQuery } from "@hazae41/glacier"
-import { Option } from "@hazae41/option"
+import { Data, Fetched, FetcherMore, QueryStorage, States, createQuery } from "@hazae41/glacier"
+import { Option, Some } from "@hazae41/option"
 import { Result } from "@hazae41/result"
 import { BgEthereumContext } from "../../../context"
 import { EthereumQueryKey } from "../../wallets/data"
 
 export namespace BgPairV3 {
+
+  export namespace Price {
+
+    export type K = EthereumQueryKey<unknown>
+    export type D = Fixed.From
+    export type F = Error
+
+    export function keyOrThrow(pair: PairData, block: string) {
+      return {
+        chainId: pair.chainId,
+        method: "eth_get",
+        params: [{
+          to: pair.address,
+          data: "price"
+        }, block]
+      }
+    }
+
+    export function queryOrThrow(context: BgEthereumContext, pair: PairData, block: string, storage: QueryStorage) {
+      const fetcher = (request: K, more: FetcherMore) => Fetched.runOrDoubleWrap(async () => {
+        const sqrtPriceX96 = await SqrtPriceX96.queryOrThrow(context, pair, block, storage).refetch().then(r => Option.wrap(r.real?.current).getOrThrow())
+
+        if (sqrtPriceX96.isErr())
+          return sqrtPriceX96
+
+        const decimals0 = tokenByAddress[pair.token0].decimals
+        const decimals1 = tokenByAddress[pair.token1].decimals
+
+        const sqrtPriceX96BigInt = Fixed.from(sqrtPriceX96.get()).value
+
+        const priceBigInt = (sqrtPriceX96BigInt / (2n ** 96n)) ** 2n
+        const ratioBigInt = (10n ** BigInt(decimals1)) / (10n ** BigInt(decimals0))
+
+        return new Data(new Fixed(priceBigInt / ratioBigInt, decimals1), sqrtPriceX96)
+      })
+
+      return createQuery<K, D, F>({
+        key: keyOrThrow(pair, block),
+        fetcher,
+        storage
+      })
+    }
+  }
 
   export namespace SqrtPriceX96 {
 
@@ -29,19 +72,44 @@ export namespace BgPairV3 {
 
     export function queryOrThrow(context: BgEthereumContext, pair: PairData, block: string, storage: QueryStorage) {
       const fetcher = (request: K, more: FetcherMore) => Fetched.runOrDoubleWrap(async () => {
-        const slot0 = await Slot0.queryOrThrow(context, pair, block, storage).refetch().then(r => Option.wrap(r.current).getOrThrow())
+        const slot0 = await Slot0.queryOrThrow(context, pair, block, storage).refetch().then(r => Option.wrap(r.real?.current).getOrThrow())
 
         if (slot0.isErr())
           return slot0
 
-        const [sqrtPriceX96] = slot0.get().intoOrThrow()
+        const [sqrtPriceX96] = slot0.get()
 
-        return new Data(new Fixed(sqrtPriceX96, 0))
+        return new Data(sqrtPriceX96, slot0)
       })
+
+      const indexer = async (states: States<D, F>) => {
+        const { current } = states
+
+        const maybeCurrentData = current.real?.current.checkOrNull()
+
+        await Price.queryOrThrow(context, pair, block, storage).mutate(() => {
+          if (maybeCurrentData == null)
+            return new Some(undefined)
+
+          const sqrtPriceX96 = maybeCurrentData
+
+          const decimals0 = tokenByAddress[pair.token0].decimals
+          const decimals1 = tokenByAddress[pair.token1].decimals
+
+          const sqrtPriceX96BigInt = Fixed.from(sqrtPriceX96.get()).value
+
+          const priceBigInt = (sqrtPriceX96BigInt / (2n ** 96n)) ** 2n
+          const ratioBigInt = (10n ** BigInt(decimals1)) / (10n ** BigInt(decimals0))
+
+          return new Some(new Data(new Fixed(priceBigInt / ratioBigInt, decimals1), sqrtPriceX96))
+        })
+      }
+
 
       return createQuery<K, D, F>({
         key: keyOrThrow(pair, block),
         fetcher,
+        indexer,
         storage
       })
     }
@@ -51,7 +119,7 @@ export namespace BgPairV3 {
   export namespace Slot0 {
 
     export type K = EthereumQueryKey<unknown>
-    export type D = Abi.Tuple.Instance<readonly [typeof Abi.Uint160, typeof Abi.Uint160, typeof Abi.Uint32, typeof Abi.Uint32, typeof Abi.Uint32, typeof Abi.Uint32]>
+    export type D = readonly [Fixed.From<0>, Fixed.From<0>, Fixed.From<0>, Fixed.From<0>, Fixed.From<0>, Fixed.From<0>]
     export type F = Error
 
     export function keyOrThrow(pair: PairData, block: string) {
@@ -73,15 +141,30 @@ export namespace BgPairV3 {
           return fetched
 
         const returns = Abi.Tuple.create(Abi.Uint160, Abi.Uint160, Abi.Uint32, Abi.Uint32, Abi.Uint32, Abi.Uint32)
-        const decoded = Abi.decodeOrThrow(returns, fetched.get())
+        const decoded = Abi.decodeOrThrow(returns, fetched.get()).intoOrThrow().map(x => new Fixed(x, 0))
 
-        return new Data(decoded)
+        return new Data(decoded as unknown as D)
       })
 
+      const indexer = async (states: States<D, F>) => {
+        const { current } = states
+
+        const maybeCurrentData = current.real?.current.checkOrNull()
+
+        await SqrtPriceX96.queryOrThrow(context, pair, block, storage).mutate(() => {
+          if (maybeCurrentData == null)
+            return new Some(undefined)
+
+          const [sqrtPriceX96] = maybeCurrentData.get()
+
+          return new Some(new Data(sqrtPriceX96, maybeCurrentData))
+        })
+      }
 
       return createQuery<K, D, F>({
         key: keyOrThrow(pair, block),
         fetcher,
+        indexer,
         storage
       })
 
