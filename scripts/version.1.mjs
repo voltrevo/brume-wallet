2b7e2151235521c097950523606d64ab926be54f
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-12-16 22:21:07 +0100

    d

--- scripts/version.1.mjs
+++ scripts/version.1.mjs
@@ -3,6 +3,8 @@ import { dirname } from "path";
 
 config({ path: dirname(new URL(import.meta.url).pathname) + "/.env.local" })
 
+console.log(process.env.IPFS_SECRET)
+
 const headers = new Headers({ "Authorization": `Bearer ${process.env.IPFS_SECRET}` })
 const response = await fetch(`https://ipfs0.hazae41.me:5001/api/v0/swarm/peers`, { method: "POST", headers })
 
