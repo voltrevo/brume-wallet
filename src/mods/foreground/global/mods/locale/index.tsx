aedc055ee3a93226e4c106db3396b429e0c89c81
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-01-25 19:48:34 +0100

    locale

--- src/mods/foreground/global/mods/locale/index.tsx
+++ src/mods/foreground/global/mods/locale/index.tsx
@@ -1,8 +1,8 @@
-import { Option } from "@hazae41/option";
+import { Nullable, Option, Some } from "@hazae41/option";
 import { createContext, useContext } from "react";
 
-export const LocaleContext = createContext<string>(navigator.language.split("-")[0])
+export const LocaleContext = createContext<Nullable<string>>(undefined)
 
 export function useLocaleContext() {
-  return Option.wrap(useContext(LocaleContext))
+  return Option.wrap(useContext(LocaleContext)).or(new Some(navigator.language.split("-")[0]))
 }
